7380759323141c43b75b611f681b5ebc02edfb85
Hisho Ohmasu <>
2020-09-19 16:52:29 +0900

    🐛 fix

--- bin/card.js
+++ bin/card.js
@@ -3,7 +3,7 @@
 
 'use strict';
 
-const fs = require('fs-extra');
+const fs = require('fs');
 const path = require('path');
 const output = fs.readFileSync(path.join(__dirname, '../dist/output'), 'utf8');
 console.log(output);
